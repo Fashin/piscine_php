be755f74845dccf21a1dc8b4d6e1c32721beeb32
Cyprian BEAUVOIS <cbeauvoi@e1r11p16.42.fr>
2017-09-17 15:08:42 +0200

    ex04 enfin FINIT

--- j02/ex04/photos.php
+++ j02/ex04/photos.php
@@ -4,34 +4,46 @@
 if ($argc == 2)
 {
   $ch = curl_init($argv[1]);
-  $filename = $argv[1] . '/tmp';
-  if (mkdir($argv[1]))
+  if (preg_match_all('/[a-zA-Z]+:\/\/(.+)/', $argv[1], $match))
   {
-    if ($fd = fopen($filename, 'w'))
+    $folder = $match[1][0];
+    $filename = $folder . '/tmp';
+    if (mkdir($folder))
     {
-      curl_setopt($ch, CURLOPT_HEADER, 0);
-      curl_setopt($ch, CURLOPT_FILE, $fd);
-      curl_exec($ch);
-      curl_close($ch);
-      fclose($fd);
-      $content = file_get_contents($filename);
-      if (preg_match_all('/<img src="([a-zA-Z\s:=\/\.0-9-_]+)"/', $content, $match))
+      if ($fd = fopen($filename, 'w'))
       {
-        $match = $match[1];
-        unlink($filename);
-        for ($i = 0; $i < count($match); $i++)
+        curl_setopt($ch, CURLOPT_HEADER, 0);
+        curl_setopt($ch, CURLOPT_FILE, $fd);
+        curl_exec($ch);
+        curl_close($ch);
+        fclose($fd);
+        $content = file_get_contents($filename);
+        if (preg_match_all('/<img.+src="([a-zA-Z\s:=\/\.0-9-_]+)"/', $content, $match))
         {
-          $filename = quotemeta($argv[1] . '\/' . $match[$i]);
-          if (!(fopen($filename, 'a+')))
-            echo "Can't create file\n";
+          $match = $match[1];
+          unlink($filename);
+          for ($i = 0; $i < count($match); $i++)
+          {
+            $file = explode('/', $match[$i]);
+            $file = $file[count($file) - 1];
+            $filename = $folder . '/' . $file;
+            if (!(fopen($filename, 'a+')))
+              echo "Can't create output file\n";
+          }
         }
+        else
+          echo "No pictures found\n";
       }
+      else
+        echo "Can't create tempory file\n";
     }
     else
-      echo "Can't create file\n";
+      echo "Error from folder create\n";
   }
   else
-    echo "Folder already exists\n";
+    echo "Enter a valid URL\n";
 }
+else
+  echo "Usage ./photo.php [path to the website]\n";
 
 ?>
